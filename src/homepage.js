b8e443fceeade8bee8a54720ebd05a977a9d709e
Jack Agtual <jaagtual@gmail.com>
2022-11-05 22:11:32 -0700

    Adding module to render homepage

---
+++ src/homepage.js
@@ -0,0 +1,3 @@
+export default renderHomepage = () => {
+    console.log('test from homepage');
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("test");
+import renderHomepage from 'homepage.js'
+
+renderHomepage();
