c0ea1cf6a6eeef44ab9201ff7dfcfb1681d47045
Jack Agtual <jaagtual@gmail.com>
2022-11-06 11:54:39 -0800

    Refactoring homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -3,24 +3,15 @@ export default function renderHomepage(content) {
     const homepage = document.createElement('div');
     homepage.classList.add('homepage');
 
-    let gridEl = document.createElement('div');
-    gridEl.classList.add('grid-el');
-    gridEl.innerHTML = `
+    const grieElContent = [
+        `
         <h1>My Restaurant</h1>
-    `;
-    homepage.appendChild(gridEl);
-
-    gridEl = document.createElement('div');
-    gridEl.classList.add('grid-el');
-    gridEl.innerHTML = `
+        `,
+        `
         <h2>About</h2>
         <p>This is the about page of our restaurant. We serve food.</p>
-    `;
-    homepage.appendChild(gridEl);
-
-    gridEl = document.createElement('div');
-    gridEl.classList.add('grid-el');
-    gridEl.innerHTML = `
+        `,
+        `
         <h2>Hours</h2>
         <ul>
             <li>Day: 9:00 - 5:00</li>
@@ -31,18 +22,20 @@ export default function renderHomepage(content) {
             <li>Day: 9:00 - 5:00</li>
             <li>Day: 9:00 - 5:00</li>
         </ul>
-    `;
-    homepage.appendChild(gridEl);
-
-    
-    gridEl = document.createElement('div');
-    gridEl.classList.add('grid-el');
-    gridEl.innerHTML = `
+        `,
+        `
         <h2>Location</h2>
         <p>We are located in Convoy San Diego</p>
         <p>1234 Convoy St. San Diego CA</p>
-    `;
-    homepage.appendChild(gridEl);
+        `
+    ]
+
+    grieElContent.forEach(htmlContent => {
+        const gridEl = document.createElement('div');
+        gridEl.classList.add('grid-el');
+        gridEl.innerHTML = htmlContent;
+        homepage.appendChild(gridEl);
+    });
 
     content.appendChild(homepage);
 }
