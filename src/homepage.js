73132cfb9998ca53761274d9a71a890677ba0667
Jack Agtual <jaagtual@gmail.com>
2022-11-06 11:49:32 -0800

    Rendering homepage in homepage.js

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,49 @@
 export default function renderHomepage(content) {
-    // const title = document.createElement('h1');
-    // title.innerText = 'Restaurant Page';
-    // content.appendChild(title);
+
+    const homepage = document.createElement('div');
+    homepage.classList.add('homepage');
+
+    let gridEl = document.createElement('div');
+    gridEl.classList.add('grid-el');
+    gridEl.innerHTML = `
+        <h1>My Restaurant</h1>
+    `;
+    homepage.appendChild(gridEl);
+
+    gridEl = document.createElement('div');
+    gridEl.classList.add('grid-el');
+    gridEl.innerHTML = `
+        <h2>About</h2>
+        <p>This is the about page of our restaurant. We serve food.</p>
+    `;
+    homepage.appendChild(gridEl);
+
+    gridEl = document.createElement('div');
+    gridEl.classList.add('grid-el');
+    gridEl.innerHTML = `
+        <h2>Hours</h2>
+        <ul>
+            <li>Day: 9:00 - 5:00</li>
+            <li>Day: 9:00 - 5:00</li>
+            <li>Day: 9:00 - 5:00</li>
+            <li>Day: 9:00 - 5:00</li>
+            <li>Day: 9:00 - 5:00</li>
+            <li>Day: 9:00 - 5:00</li>
+            <li>Day: 9:00 - 5:00</li>
+        </ul>
+    `;
+    homepage.appendChild(gridEl);
+
+    
+    gridEl = document.createElement('div');
+    gridEl.classList.add('grid-el');
+    gridEl.innerHTML = `
+        <h2>Location</h2>
+        <p>We are located in Convoy San Diego</p>
+        <p>1234 Convoy St. San Diego CA</p>
+    `;
+    homepage.appendChild(gridEl);
+
+    content.appendChild(homepage);
 }
+
