00b713161fa68ab6f212bc0b53af8c06b3e62b52
Jack Agtual <jaagtual@gmail.com>
2022-11-11 10:44:29 -0800

    Switching pages when navigation buttons are clicked

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import './styles.css'
-import renderHomepage from './homepage.js'
+import renderHomepage from './homepage'
 import renderMenu from './renderMenu';
+import clearScreen from './clearScreen';
 
 const content = document.querySelector('#content');
+const homeBtn = document.querySelector('#home-btn');
+const menuBtn = document.querySelector('#menu-btn');
 
-// renderHomepage(content);
-renderMenu(content);
+renderHomepage(content);
+
+homeBtn.addEventListener('click', () => {
+    clearScreen(content)
+    renderHomepage(content)
+});
+menuBtn.addEventListener('click', () => {
+    clearScreen(content)
+    renderMenu(content)
+});
